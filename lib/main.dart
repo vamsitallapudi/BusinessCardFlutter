e0076191ceb4d7b98e1369c0a175a7097f42ee40
Vamsi Tallapudi <vamsi@coderefer.com>
2020-09-10 09:45:52 +0530

    Business card my final changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,67 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blue,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
                 radius: 50.0,
-                backgroundImage: AssetImage('assets/profile_pic.jpg')),
+                backgroundImage: AssetImage('images/profile_pic.jpg')),
+            Text(
+              'Vamsi Tallapudi',
+              style: TextStyle(
+                fontFamily: 'Kaushan Script',
+                color: Colors.white70,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                fontSize: 40.0,
+              ),
+            ),
+            Text(
+              'Technical Lead',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.white,
+                fontSize: 25.0,
+              ),
+            ),
+            SizedBox(
+              height: 32.0,
+              width: 150.0,
+              child: Divider(
+                thickness: 1.0,
+                color: Colors.white54,
+              ),
+            ),
+            Card(
+              color: Colors.pink,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.orange,
+                ),
+                title: Text(
+                  '+91 8686670474',
+                  style: TextStyle(
+                      color: Colors.white70, fontFamily: 'Source Sans Pro'),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.pink,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.orange,
+                ),
+                title: Text(
+                  'vamsi@coderefer.com',
+                  style: TextStyle(
+                      color: Colors.white70, fontFamily: 'Source Sans Pro'),
+                ),
+              ),
+            ),
           ],
         )),
       ),
